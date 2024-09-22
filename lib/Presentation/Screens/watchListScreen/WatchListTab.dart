dcf773db91f29be9c099816da6e564447985d276
ziad ramy <ziadr9687@gmail.com>
2024-09-22 18:33:07 +0300

    edit on design

--- lib/Presentation/Screens/watchListScreen/WatchListTab.dart
+++ lib/Presentation/Screens/watchListScreen/WatchListTab.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/Shared/Text_Theme.dart';
 
-
 class WatchListTab extends StatelessWidget {
   static const String routename = 'watchlisttab';
   final List<String> movieImages = [
-    'assets/dora.jpeg',
+    'assets/imagetitle.png',
     'assets/dora.jpeg',
     'assets/dora.jpeg',
     'assets/dora.jpeg',
@@ -39,72 +37,83 @@ class WatchListTab extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text('Watchlist',style: TextThemee.bodyLargeWhite,),
+          title: Text(
+            'Watchlist',
+            style: TextThemee.bodyLargeWhite,
+          ),
           backgroundColor: Colors.black,
         ),
         body: ListView.builder(
           itemCount: movieImages.length,
           itemBuilder: (context, index) {
-            return _buildWatchlistItem(
-                movieImages[index],
-                movieTitles[index],
-                movieDescriptions[index]
-            );
+            return _buildWatchlistItem(movieImages[index], movieTitles[index],
+                movieDescriptions[index]);
           },
         ),
-        
       ),
     );
   }
 
-  Widget _buildWatchlistItem(String imagePath, String title, String description) {
-    return Card(
-      color: Colors.black,
-      child: ListTile(
-        leading:  Stack(
-                                    children: [
-                                      Container(
-                                        child: Image.asset(
-                                          imagePath,
-                                          height: 200.h,
-                                          width: 150.w,
-                                        ),
-                                      ),
-                                      Positioned(
-                                        top: -8.h,
-                                        left: -11.w,
-                                        child: IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.bookmark_add_outlined,
-                                            color: Colors.white,
-                                            size: 30.sp,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-        title: Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-        ),
-        subtitle: Text(
-          description,
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 14,
-            fontStyle: FontStyle.normal,
-          ),
-        ),
-        trailing: Icon(
-          Icons.check_box,
-          color: Colors.yellow,
-        ),
-      ),
-    );
+  Widget _buildWatchlistItem(
+      String imagePath, String title, String description) {
+    return Container(
+      padding: EdgeInsets.all(5),
+        height: 100.h,
+        margin: EdgeInsets.only(bottom: 15),
+        color: Colors.black,
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  width: 200.w,
+                  height: 200.h,
+                  child: Image.asset(
+                    imagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: -8.h,
+                  left: -11.w,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.bookmark_add_outlined,
+                      color: Colors.white,
+                      size: 30.sp,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              
+              width: 200.w,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    description.substring(0, 30),
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
